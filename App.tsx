cc4f66f823809dba6250bc5ed5ebfd510a458d75
svaldessamperio <svaldes_samperio@hotmail.com>
2022-01-31 21:33:14 -0600

    Se agrega manejo de notificaciones foreground, background, topics

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './src/navigation/Navigator';
-import { LogBox } from 'react-native';
+import { Alert, AppRegistry, LogBox } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext';
+import messaging from '@react-native-firebase/messaging';
+
+// Register background handler
+const backgroudSubscriber = messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+AppRegistry.registerComponent('app', () => App);
 
 export default function App() {
   LogBox.ignoreLogs([
@@ -11,6 +19,32 @@ export default function App() {
     "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
   ]);
   
+
+  useEffect(() => {
+    
+    const token = messaging().getToken().then((token) => {
+      console.log('Token: ' + token);
+    });
+
+    const foregroundSubscriber = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    const topicSubscriber = messaging().subscribeToTopic("Pagos").then( ()=> {
+      console.log("Suscrito al Tópico");    
+    });
+
+
+    return () => {
+      topicSubscriber;
+      foregroundSubscriber();
+      backgroudSubscriber;
+    };
+
+  }, []);
+
+
   return (
     <NavigationContainer>{/* Rest of your app code */}
       <AppState>
